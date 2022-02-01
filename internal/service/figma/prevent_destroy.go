160780c89db82a0f83b6bdc9b81c3d73a4a6429c
David Mah <dmah@figma.com>
2022-01-31 16:45:41 -0800

    gofmt

--- internal/service/figma/prevent_destroy.go
+++ internal/service/figma/prevent_destroy.go
@@ -1,12 +1,12 @@
 package figma
 
 import (
-  "strconv"
 	"fmt"
 	"log"
+	"math/rand"
+	"os"
+	"strconv"
 	"time"
-  "os"
-  "math/rand"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -16,8 +16,8 @@ func ResourcePreventDestroy() *schema.Resource {
 		Create: ResourcePreventDestroyCreate,
 		Read:   ResourcePreventDestroyRead,
 		Delete: ResourcePreventDestroyDelete,
-    // Update: Not needed or allowed because there are no callsite
-    // controllable parameters
+		// Update: Not needed or allowed because there are no callsite
+		// controllable parameters
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -29,33 +29,33 @@ func ResourcePreventDestroy() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-      "id": {
+			"id": {
 				Type:     schema.TypeString,
-        Computed: true,
-      },
-    },
+				Computed: true,
+			},
+		},
 	}
 }
 
 func ResourcePreventDestroyCreate(d *schema.ResourceData, meta interface{}) error {
-  d.SetId(strconv.Itoa(rand.Int()))
+	d.SetId(strconv.Itoa(rand.Int()))
 	return ResourcePreventDestroyRead(d, meta)
 }
 
 func ResourcePreventDestroyRead(d *schema.ResourceData, meta interface{}) error {
-  return nil
+	return nil
 }
 
 func ResourcePreventDestroyDelete(d *schema.ResourceData, meta interface{}) error {
-  if os.Getenv("TF_PREVENT_DESTROY") != "false" {
-    return fmt.Errorf(
-      "Destroy blocked on prevent-destroy module." +
-      " We create these to act as guard rails that protect against accidental destruction of important resources." +
-      " Please check your plan and make sure you are not destroying anything important." +
-      " If you really mean to destroy, please set env var TF_PREVENT_DESTROY=false and re-run.")
-  } else {
+	if os.Getenv("TF_PREVENT_DESTROY") != "false" {
+		return fmt.Errorf(
+			"Destroy blocked on prevent-destroy module." +
+				" We create these to act as guard rails that protect against accidental destruction of important resources." +
+				" Please check your plan and make sure you are not destroying anything important." +
+				" If you really mean to destroy, please set env var TF_PREVENT_DESTROY=false and re-run.")
+	} else {
 		log.Printf("[INFO] TF_PREVENT_DESTROY=false passed, so allowing destroy")
-  }
+	}
 
-  return nil
+	return nil
 }
